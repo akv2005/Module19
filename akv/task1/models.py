141becea0e8e000744b0a09df86cb1baaa04777c
akv05 <akv0571@gmail.com>
2024-09-20 00:02:24 +0900

    Домашнее задание по теме "Модели баз данных в Django

--- akv/task1/models.py
+++ akv/task1/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=30)
-    balance = models.DecimalField(max_digits=20, decimal_places=2)
+    name = models.CharField(max_length=50)
+    balance = models.DecimalField(max_digits=30, decimal_places=2)
     age = models.IntegerField()
 
     def __str__(self):
@@ -13,8 +13,8 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=20, decimal_places=2)
-    size = models.DecimalField(max_digits=20, decimal_places=3)
+    cost = models.DecimalField(max_digits=30, decimal_places=2)
+    size = models.DecimalField(max_digits=30, decimal_places=3)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='buyers')
