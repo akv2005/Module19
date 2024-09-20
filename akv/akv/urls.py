3961d4d1a345c41d9adf1ed727e84ba4e7e4f4eb
akv05 <akv0571@gmail.com>
2024-09-21 00:58:03 +0900

    Домашнее задание по теме "MVT. Вывод объектов в шаблоны".

--- akv/akv/urls.py
+++ akv/akv/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path
+#from task2.views import class2, func2
+from task1.views import cart, games, main, sign_up_by_django, sign_up_by_html
+#from task5.views import sign_up_by_django, sign_up_by_html
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+#    path('class/', class2.as_view()),
+#    path('func/', func2),
+    path('', TemplateView.as_view(template_name='fourth_task/menu.html')),
+    path('cart/', cart),
+    path('games/', games),
+    path('django_sign_up/', sign_up_by_django),
+    path('django_sign/', sign_up_by_html),
 ]
--- akv/task1/views.py
+++ akv/task1/views.py
@@ -1,3 +1,104 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .forms import UserRegister
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+class main(TemplateView):
+    template_name = 'platform.html'
+
+def cart(request):
+    mydict = {'games': ["Atomic Heart", "Cyberpunk 2077", "ZX Spectrum 1982"]}
+    context = {
+        'mydict': mydict,
+    }
+
+    return render(request, 'fourth_task/cart.html', context)
+
+
+def games(request):
+    context = {
+        'back': '/',
+        'cart': '/cart'
+    }
+    return render(request, template_name='fourth_task/games.html', context=context)
+
+
+def sign_up_by_html(request):
+    i = 0
+    info = {'error':[]}
+    users = Buyer.objects.all().values_list('name', flat=True)
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeate_password = request.POST.get('repeate_password')
+        age = request.POST.get('age')
+
+        print(f'Username: {username}')
+        print(f'Password: {password}')
+        print(f'Repeate password: {repeate_password}')
+        print(f'Age: {age}')
+        if username not in users and password == repeate_password and int(age)>=18:
+            Buyer.objects.create(name=username, balance=10, age=age)
+            print(users)
+            return HttpResponse(f'Приветствуем {username}')
+        elif username in users:
+            i +=1
+            info[f'error {i}'] = HttpResponse('Этот логин уже занят', status=400, reason='repeated login')
+            return HttpResponse('Этот логин уже занят', status=400, reason='repeated login')
+        elif password != repeate_password:
+            i +=1
+            info[f'error {i}'] = HttpResponse('Пароли не совпадают', status=400, reason='non-identical passwords')
+            return HttpResponse('Пароли не совпадают', status=400, reason='non-identical passwords')
+        elif int(age) < 18:
+            i +=1
+            info[f'error {i}'] = HttpResponse(f'Регистрация разрешена с 18ти лет. ', status=400, reason='insufficient age')
+            return HttpResponse(f'Регистрация разрешена с 18ти лет.', status=400, reason='insufficient age')
+    context = {'info':info}
+
+    print(users,'!!!!!')
+    return render(request, 'fifth_task/registration_page.html', context)
+
+
+def sign_up_by_django(request):
+    info = {'error': []}
+    i = 0
+    users = Buyer.objects.all().values_list('name', flat=True)
+    if request.method == "POST":
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeate_password = form.cleaned_data['repeate_password']
+            age = form.cleaned_data['age']
+            if username not in users and password == repeate_password and int(age) >= 18:
+                Buyer.objects.create(name=username, balance=10, age=age)
+                return HttpResponse(f'Приветствуем {username}')
+            elif username in users:
+                i += 1
+                info[f'error {i}'] = HttpResponse('Этот логин уже занят', status=400, reason='repeated login')
+                print(info[f'error {i}'])
+                return HttpResponse('Этот логин уже занят', status=400, reason='repeated login')
+            elif password != repeate_password:
+                i += 1
+                info[f'error {i}'] = HttpResponse('Пароли не совпадают', status=400, reason='repeated login')
+                print(info[f'error {i}'])
+                return HttpResponse('Пароли не совпадают', status=400, reason='non-identical passwords')
+            elif int(age) < 18:
+                i += 1
+                info[f'error {i}'] = HttpResponse(
+                    f'Регистрация разрешена с 18ти лет.', status=400,
+                    reason='insufficient age')
+
+                return HttpResponse(
+                    f'Регистрация разрешена с 18ти лет. ', status=400,
+                    reason='insufficient age')
+
+    else:
+
+        form = UserRegister()
+        context = {'info': info, 'form': form}
+        return render(request, 'fifth_task/registration_page.html', context)
+
+
+
